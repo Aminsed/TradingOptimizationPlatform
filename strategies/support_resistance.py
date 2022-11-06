7498a231e9c6c0079364cc63ca999d081abd4347
Amin Sedaghat <amin32846@gmail.com>
2022-11-05 22:18:08 -0400

    removed mlpfinance

--- strategies/support_resistance.py
+++ strategies/support_resistance.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 
 
-import matplotlib.pyplot as plt
-import mplfinance as mpf
+# import matplotlib.pyplot as plt
+# import mplfinance as mpf
 
 pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
