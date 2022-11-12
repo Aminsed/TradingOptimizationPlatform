d62bd4d3431080833d0e2b9a392ede283ca9dbe9
Amin Sedaghat <amin32846@gmail.com>
2022-11-12 15:51:55 -0500

    fixed dd error

--- strategies/sma_sl_tp.py
+++ strategies/sma_sl_tp.py
@@ -144,5 +144,7 @@ def backtest(data: pd.core.frame.DataFrame, slow_ma_period: int, fast_ma_period:
                 pending_order = {"order_id":i, "trade_side":-1, "trade_entry_price":trade_entry_price,
                                 "stoploss":sl, "takeprofit":tp}
     
-
-    return balance, number_of_trades
+    if number_of_trades == 0:
+        return 0, 0
+    else:
+        return balance, number_of_trades
