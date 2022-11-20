f1e580f9e6218342cbb46ccb277a039847a79794
Amin Sedaghat <amin32846@gmail.com>
2022-11-20 17:04:57 -0500

    debugging

--- strategies/sma_sl_tp.py
+++ strategies/sma_sl_tp.py
@@ -10,14 +10,24 @@ pd.set_option("display.width", 1000)
 
 def backtest(data1: pd.core.frame.DataFrame, slow_ma_period: int, fast_ma_period: int,takeprofit: float ,stoploss: float) -> typing.Tuple[float, float]:
     data = data1.copy()
+ #   data = data.dropna()
+
     data['low'] = data['low'].astype('float')
     data['high'] = data['high'].astype('float')
     data['close'] = data['close'].astype('float')
+#    data = data.dropna()
+
     data['slow_ma'] = data['close'].rolling(window=slow_ma_period).mean()
+#    data = data.dropna()
+    
     data['fast_ma'] = data['close'].rolling(window=fast_ma_period).mean()
+ #   data = data.dropna()
+    
     data['atr'] = talib.ATR(data['high'], data['low'], data['close'], timeperiod=fast_ma_period)
+ #   data = data.dropna()
+
     data["signal"] = np.where(data["fast_ma"] > data["slow_ma"], 1, -1)
-    data = data.dropna()
+
 
     
     
