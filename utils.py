e8281b65363527043c4b0f169aac5b286b4277b9
Amin Sedaghat <amin32846@gmail.com>
2023-01-09 20:05:29 -0500

    modified slow ma range to 10000

--- utils.py
+++ utils.py
@@ -51,7 +51,7 @@ STRAT_PARAMS = {
         "stoploss": {"name": "Stop Loss", "type": float, "min": 2.00, "max": 50, "decimals": 2},
     },
     "sma_sl_tp_fixed": {
-        "slow_ma_period": {"name": "Slow MA Period", "type": int, "min": 1, "max": 2000},
+        "slow_ma_period": {"name": "Slow MA Period", "type": int, "min": 1, "max": 10000},
         "fast_ma_period": {"name": "Fast MA Period", "type": int, "min": 1, "max": 2000},
         "atr_period": {"name": "ATR Period", "type": int, "min": 100, "max": 2000},
         "takeprofit": {"name": "Take Profit", "type": float, "min": 2.00, "max": 50, "decimals": 2},
