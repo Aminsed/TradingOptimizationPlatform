43163fdaae918f39c31db6d896bc850be649ec4a
Amin Sedaghat <amin32846@gmail.com>
2023-09-19 00:56:57 +0000

    fixed hist data get

--- exchanges/forexdotcom.py
+++ exchanges/forexdotcom.py
@@ -1,82 +1,79 @@
 import requests
+import json
 import logging
 
 logger = logging.getLogger()
 
-class CityIndexClient:
-    def __init__(self, username, password, app_key):
-        self._base_url = "https://ciapi.cityindex.com"
-        self.username = username
-        self.password = password
-        self.app_key = app_key
-        self._session = self._login()
-        self.symbols = self._get_symbols()
+class GCapiClient:
+    def __init__(self, username, password, appkey):
+        self._base_url = "https://ciapi.cityindex.com/TradingAPI"
+        self._session = self._create_session(username, password, appkey)
 
-    def _make_request(self, endpoint: str, params: dict = None, method: str = 'GET', json: dict = None):
-        url = self._base_url + endpoint
+    def _create_session(self, username, password, appkey):
+        headers = {'Content-Type': 'application/json'}
+        data = {
+            "UserName": username,
+            "Password": password,
+            "AppKey": appkey
+        }
         try:
-            if method == 'GET':
-                response = requests.get(url, params=params)
-            elif method == 'POST':
-                response = requests.post(url, json=json)
-            if response.status_code == 200:
-                return response.json()
-            else:
-                logger.error("Request failed: %s", response.json())
-                return None
+            response = requests.post(self._base_url + '/session', headers=headers, json=data)
+            response.raise_for_status()
+            resp = response.json()
+            if resp['StatusCode'] != 1:
+                raise Exception(resp)
+            session = resp['Session']
+            headers = {
+                'Content-Type': 'application/json',
+                'UserName': username,
+                'Session': session
+            }
+            s = requests.Session()
+            s.headers = headers
+            return s
         except Exception as e:
-            logger.error("Connection error: %s", e)
+            logger.error("Error while creating session: %s", e)
             return None
 
-    def _login(self):
-        login_endpoint = "/v2/Session"
-        login_data = {
-            "UserName": self.username,
-            "Password": self.password,
-            "AppKey": self.app_key,
-            "AppVersion": "1",
-            "AppComments": ""
-        }
-        response = self._make_request(login_endpoint, method='POST', json=login_data)
-        if response is not None:
-            return response["Session"]
-        else:
+    def _make_request(self, endpoint: str, query_parameters: dict = None):
+        if query_parameters is None:
+            query_parameters = {}
+        try:
+            response = self._session.get(self._base_url + endpoint, params=query_parameters)
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            logger.error("Error while making request to %s: %s", endpoint, e)
             return None
 
-    def _get_symbols(self):
-        market_endpoint = "/TradingAPI/spread/markets"
-        market_params = {
-            "maxresults": 500
-        }
-        markets = self._make_request(market_endpoint, params=market_params)
-        if markets is not None:
-            symbols = {market['MarketName']: market['MarketId'] for market in markets['Markets']}
-            return symbols
-        else:
-            return None
-            
-    def get_historical_data(self, symbol, start_time=None, end_time=None, interval="HOUR", span=1, max_results=1000):
-        # Look up the market ID for the given symbol
-        market_id = self.symbols.get(symbol)
-        if market_id is None:
-            logger.error("Symbol not found: %s", symbol)
+    def get_market_info(self, market_name):
+        endpoint = f'/cfd/markets?MarketName={market_name}'
+        return self._make_request(endpoint)
+
+    def get_prices(self, market_id, num_ticks=1, price_type='MID'):
+        endpoint = f'/market/{market_id}/tickhistory?PriceTicks={num_ticks}&priceType={price_type}'
+        return self._make_request(endpoint)
+
+    def get_historical_data(self, symbol: str, start_time: Optional[int] = None, end_time: Optional[int] = None):
+        # Get market_id from symbol
+        market_info = self.get_market_info(symbol)
+        try:
+            market_id = market_info['Markets'][0]['MarketId']
+        except (KeyError, IndexError):
             return None
 
-        historical_data_endpoint = f"/TradingAPI/market/{market_id}/barhistorybetween"
-        historical_data_params = {
-            "interval": interval,
-            "span": span,
-            "fromTimestampUTC": start_time,
-            "toTimestampUTC": end_time,
-            "maxResults": max_results
-        }
-        historical_data = self._make_request(historical_data_endpoint, params=historical_data_params)
-        if historical_data is not None:
-            candles = []
-            for bar in historical_data['PriceBars']:
-                # Convert times from "/Date(x)/" format to epoch time
-                timestamp = int(bar['BarDate'][6:-2]) / 1000
-                candles.append((timestamp, bar['Open'], bar['High'], bar['Low'], bar['Close'], bar['Volume']))
-            return candles
+        # If start_time and end_time are not provided, get the last 5000 bars
+        if start_time is None or end_time is None:
+            num_ticks = 5000
+            endpoint = f'/market/{market_id}/barhistory?PriceBars={num_ticks}'
+            return self._make_request(endpoint)
+
+        # If start_time and end_time are provided, get historical data between these timestamps
         else:
-            return None
+            params = {
+                'fromTimeStampUTC': start_time,
+                'toTimestampUTC': end_time,
+                'maxResults': 4000
+            }
+            endpoint = f'/market/{market_id}/tickhistorybetween'
+            return self._make_request(endpoint, params)
