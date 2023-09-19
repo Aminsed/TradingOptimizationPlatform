c130666fbbea21331dee862b86eaf9d67da2c888
Amin Sedaghat <amin32846@gmail.com>
2023-09-19 01:23:49 +0000

    corrected timestamp format and added custom exception class

--- exchanges/forexdotcom.py
+++ exchanges/forexdotcom.py
@@ -1,8 +1,38 @@
 import requests
 import json
 import logging
+from typing import Optional
 
 logger = logging.getLogger()
+
+
+class GCapiException(Exception):
+    def __init__(self, exception):
+        self.exception = exception
+
+    def get_exception(self):
+        return self.exception
+
+    def get_error_message(self):
+        if self.exception['ErrorMessage']:
+            return self.exception['ErrorMessage']
+
+    def get_status_code(self):
+        if self.exception['StatusCode']:
+            return self.exception['StatusCode']
+
+    def get_additional_info(self):
+        if self.exception['AdditionalInfo']:
+            return self.exception['AdditionalInfo']
+
+    def get_http_status(self):
+        if self.exception['HttpStatus']:
+            return self.exception['HttpStatus']
+
+    def get_error_code(self):
+        if self.exception['ErrorCode']:
+            return self.exception['ErrorCode']
+
 
 class GCapiClient:
     def __init__(self, username, password, appkey):
@@ -55,25 +85,20 @@ class GCapiClient:
         return self._make_request(endpoint)
 
     def get_historical_data(self, symbol: str, start_time: Optional[int] = None, end_time: Optional[int] = None):
-        # Get market_id from symbol
-        market_info = self.get_market_info(symbol)
-        try:
-            market_id = market_info['Markets'][0]['MarketId']
-        except (KeyError, IndexError):
-            return None
-
-        # If start_time and end_time are not provided, get the last 5000 bars
-        if start_time is None or end_time is None:
-            num_ticks = 5000
-            endpoint = f'/market/{market_id}/barhistory?PriceBars={num_ticks}'
-            return self._make_request(endpoint)
-
-        # If start_time and end_time are provided, get historical data between these timestamps
+        if start_time is not None and end_time is not None:
+            r = self._session.get(
+                self._base_url + f'/market/{symbol}/tickhistorybetween?fromTimeStampUTC={start_time}&toTimestampUTC={end_time}&priceType=MID')
         else:
-            params = {
-                'fromTimeStampUTC': start_time,
-                'toTimestampUTC': end_time,
-                'maxResults': 4000
-            }
-            endpoint = f'/market/{market_id}/tickhistorybetween'
-            return self._make_request(endpoint, params)
+            r = self._session.get(self._base_url + f'/market/{symbol}/tickhistory?PriceTicks=5000&priceType=MID')
+        resp = json.loads(r.text)
+        prices = []
+        try:
+            for price in resp['PriceTicks']:
+                # Extract timestamp from the string and convert it to float
+                timestamp = float(price['TickDate'][6:-2]) / 1000
+                # Append tuple (timestamp, price, price, price, price, volume)
+                prices.append((timestamp, price['Price'], price['Price'], price['Price'], price['Price'], 0))
+            return prices
+        except:
+            raise GCapiException(resp)
+
